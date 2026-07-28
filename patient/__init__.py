cea97ba493ce9084a32232009a6347b9501f3dd3
fixer-vikas <127896317+fixer-vikas@users.noreply.github.com>
2026-07-29 00:01:52 +0530

    Delete patient directory

--- patient/__init__.py
+++
@@ -1,9 +0,0 @@
-from .services import PatientService
-
-
-def register_patient_routes(*args, **kwargs):
-    from .routes import register_patient_routes as _register_patient_routes
-    return _register_patient_routes(*args, **kwargs)
-
-
-__all__ = ["PatientService", "register_patient_routes"]
--- patient/validators.py
+++
@@ -1,5 +0,0 @@
-from typing import Any, Dict, Optional
-
-
-def validate_patient_payload(payload: Optional[Dict[str, Any]] = None) -> Dict[str, Any]:
-    return dict(payload or {})
