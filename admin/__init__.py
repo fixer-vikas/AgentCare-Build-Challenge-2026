041e8a40d982fa9122459b487398fcfcb1a96ae8
fixer-vikas <127896317+fixer-vikas@users.noreply.github.com>
2026-07-28 23:59:36 +0530

    Delete admin directory

--- admin/__init__.py
+++
@@ -1,9 +0,0 @@
-from .services import AdminService
-
-
-def register_admin_routes(*args, **kwargs):
-    from .routes import register_admin_routes as _register_admin_routes
-    return _register_admin_routes(*args, **kwargs)
-
-
-__all__ = ["AdminService", "register_admin_routes"]
