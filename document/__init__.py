3ef1bcf470c891b519be7b626e06a7e024adc6d6
fixer-vikas <127896317+fixer-vikas@users.noreply.github.com>
2026-07-29 00:00:53 +0530

    Delete document directory

--- document/__init__.py
+++
@@ -1,9 +0,0 @@
-from .services import DocumentService
-
-
-def register_document_routes(*args, **kwargs):
-    from .routes import register_document_routes as _register_document_routes
-    return _register_document_routes(*args, **kwargs)
-
-
-__all__ = ["DocumentService", "register_document_routes"]
