aac378befbba4c9cc9c5c095a1e5c12c0bf2410b
fixer-vikas <127896317+fixer-vikas@users.noreply.github.com>
2026-07-29 00:01:01 +0530

    Delete dashboard directory

--- dashboard/__init__.py
+++
@@ -1,9 +0,0 @@
-from .services import DashboardService
-
-
-def register_dashboard_routes(*args, **kwargs):
-    from .routes import register_dashboard_routes as _register_dashboard_routes
-    return _register_dashboard_routes(*args, **kwargs)
-
-
-__all__ = ["DashboardService", "register_dashboard_routes"]
