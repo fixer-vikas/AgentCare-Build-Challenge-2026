7c1eb287c6e8a4086da9654e8ed459f36eaf8691
fixer-vikas <127896317+fixer-vikas@users.noreply.github.com>
2026-07-29 00:01:17 +0530

    Delete auth directory

--- auth/__init__.py
+++
@@ -1,9 +0,0 @@
-from .services import AuthService
-
-
-def register_auth_routes(*args, **kwargs):
-    from .routes import register_auth_routes as _register_auth_routes
-    return _register_auth_routes(*args, **kwargs)
-
-
-__all__ = ["AuthService", "register_auth_routes"]
--- auth/models.py
+++
@@ -1,7 +0,0 @@
-from typing import Any, Dict, Optional
-
-
-class AuthUser(dict):
-    """Simple container for auth-related user state."""
-
-    pass
