92912981dcead101df0a08f3685a369c08dd746c
fixer-vikas <127896317+fixer-vikas@users.noreply.github.com>
2026-07-28 16:42:26 +0530

    Delete AgentCare-Build-Challenge-2026 directory

--- AgentCare-Build-Challenge-2026/patient/schemas.py
+++
@@ -1,5 +0,0 @@
-from typing import Any, Dict, Optional
-
-
-def patient_request_payload(payload: Optional[Dict[str, Any]] = None) -> Dict[str, Any]:
-    return dict(payload or {})
--- AgentCare-Build-Challenge-2026/workflow/__init__.py
+++
@@ -1,9 +0,0 @@
-from .services import WorkflowService
-
-
-def register_workflow_routes(*args, **kwargs):
-    from .routes import register_workflow_routes as _register_workflow_routes
-    return _register_workflow_routes(*args, **kwargs)
-
-
-__all__ = ["WorkflowService", "register_workflow_routes"]
